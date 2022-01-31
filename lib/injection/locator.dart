dbc5b74024a0e799ef4cfd8068ada0455469b26c
Pro Lay <62893105+prolaymm@users.noreply.github.com>
2022-01-31 13:20:15 +0630

    Update locator.dart

--- lib/injection/locator.dart
+++ lib/injection/locator.dart
@@ -11,14 +11,11 @@ import 'package:flutter_mvvm/viewModels/home_view_model.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
-
 void setup(){
-
   locator.registerLazySingleton(() => ApiService(Dio(BaseOptions(contentType: "application/json"))));
   locator.registerLazySingleton(() => PostApiService(Dio(BaseOptions(contentType: "application/json"))));
  // locator.registerLazySingleton(() => ApiService(locator.get<Dio>()));
   locator.registerLazySingleton<HomeRipo>(() => HomeRipoImp(locator.get<ApiService>()));
   locator.registerLazySingleton<PostRipo>(() => PostRipoImp(locator.get<PostApiService>()));
   locator.registerFactory(() => HomeviewMoel(locator.get<HomeRipo>(),locator.get<PostRipo>()));
-
 }
